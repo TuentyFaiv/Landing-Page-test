d4c8c7b944857460ece219b968ab8fdd45570f2b
Tonalli López <aguilablanca_7@live.com>
2021-03-10 16:48:57 -0600

    [Add] landing routes

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header>Header</header>
+      {children}
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default Layout;
